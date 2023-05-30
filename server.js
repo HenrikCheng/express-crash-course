e031938b7da59057999fdddaebcda36ae995cbd2
HenrikCheng <henrikcheng@live.se>
2023-05-30 14:15:54 +0200

    how to display static files with express middleware

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const app = express();
 // Set app variables
 app.set("view engine", "ejs");
 app.use(logger);
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/demo", (req, res) => {
   console.log("Here");
   //   res.sendStatus(500) // Status only
   // res.status(500).send("Hi"); // Status and message
   // res.download("server.js"); // Download a file
   // res.status(500).send({ mesage: "Error message" }); // Status and JSON
-  res.render("index", { text: " will cause a exception" });
+  res.render("demo", { text: " will cause a exception" });
 });
 
 app.get("/posts", (req, res) => {
