613aed7f55eabd059a73cf6b11bc53e277405aa2
HenrikCheng <henrikcheng@live.se>
2023-05-30 10:40:37 +0200

    middleware+chain request users

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,52 @@ router.get("/", (req, res) => {
 });
 
 router.get("/new", (req, res) => {
+  res.send("User new form");
+});
+
+router.post("/create", function (req, res) {
   res.send("create user");
 });
+
+// Chain together all requests
+router
+  .route("/:userId")
+  .get((req, res) => {
+    const userId = req.params.userId;
+    console.log("request.user: ", req.users);
+    res.send(`Get user with id: ${userId}`);
+  })
+  .put((req, res) => {
+    const userId = req.params.userId;
+    res.send(`Update user with id: ${userId}`);
+  })
+  .delete((req, res) => {
+    const userId = req.params.userId;
+    res.send(`Delete user with id: ${userId}`);
+  });
+
+const users = [{ name: "Kyle" }, { name: "Henrik" }];
+router.param("userId", (req, res, next, userId) => {
+  req.users = users[userId];
+  next();
+});
+
+// // Dynamic route id
+// router.get("/:userId", (req, res) => {
+//   const userId = req.params.userId;
+//   res.send(`Get user with id: ${userId}`);
+// });
+
+// // Update user
+// router.put("/:userId", (req, res) => {
+//   const userId = req.params.userId;
+//   res.send(`Update user with id: ${userId}`);
+// });
+
+// // Delete user
+// router.delete("/:userId", (req, res) => {
+//   const userId = req.params.userId;
+//   res.send(`Delete user with id: ${userId}`);
+// });
 
 module.exports = router;
